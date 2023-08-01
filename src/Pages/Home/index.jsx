96ea7d343c77a3e706558dbe14d3f39ce54e6915
ldrl <drdanielreyes5@gmail.com>
2023-07-31 19:05:55 -0600

    Product Detail

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import Card from "../../Components/Card"
 import Layout from "../../Components/Layout"
+import ProductDetail from "../../Components/ProductDetail"
 const Home = () => {
   const [items, setItems] = useState(null)
 
@@ -20,6 +21,7 @@ const Home = () => {
           ))
         }
       </div>
+      <ProductDetail />
     </Layout>
   )
 }
